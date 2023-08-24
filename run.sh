d40faf95066dbf9a19edef89592870f92d7f27dc
cmark0v <cmark0v@noreply>
2023-08-24 23:39:07 +0900

    merge fix dl

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #docker network inspect CORD||docker network create CORD
-docker run --rm -it --name discord --device /dev/snd -v /etc/localtime:/etc/localtime:ro -v ~/.config/discord/:/home/dubuntu/.config/discord/ --net CORD  discord:latest 
+docker run --rm -it --name discord --device /dev/snd -v /etc/localtime:/etc/localtime:ro -v ~/.config/discord/:/home/dubuntu/.config/discord/ -v $PWD/share:/home/dubuntu/share --net CORD  discord:latest 
 
